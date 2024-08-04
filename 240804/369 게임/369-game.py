17d9474aeb9c13ed5aab6daf39d9a755a5d4809e
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2024-08-04 20:40:21 +0900

    Title: 369 게임, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 240804/369 게임/369-game.py
+++ 240804/369 게임/369-game.py
@@ -1,6 +1,6 @@
 n = int(input())
 for i in range(1, n+1):
-    if i%3==0 or (i>10 and ((i//10)%3==0 or (i%10)%3==0)):
+    if i%3==0 or (i//10!=0 and (i//10)%3==0 or i%10!=0 and (i%10)%3==0):
         print(0, end=' ')
     else:
         print(i, end=' ')
