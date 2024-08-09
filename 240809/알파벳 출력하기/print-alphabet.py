3f94edd481826d63867b11b06b03faa0a6e65832
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2024-08-09 19:17:49 +0900

    Title: 알파벳 출력하기, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240809/알파벳 출력하기/print-alphabet.py
+++ 240809/알파벳 출력하기/print-alphabet.py
@@ -4,6 +4,6 @@ for i in range(n):
     for j in range(i+1):
         print(chr(alpha), end='')
         alpha+=1
-        if alpha>=ord('A')+25:
+        if alpha>=ord('A')+26:
             alpha=ord('A')
     print()
