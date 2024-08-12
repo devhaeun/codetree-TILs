5e66a6a9c0c16bab1aab3f00fb12ee16e96b7c64
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2024-08-12 21:19:39 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240812/시험 통과 여부 확인하기 2/verify-test-passed-2.py
+++ 240812/시험 통과 여부 확인하기 2/verify-test-passed-2.py
@@ -2,7 +2,7 @@ n = int(input())
 num = 0
 for i in range(n):
     arr = list(map(int, input().split()))
-    if sum(arr)/4>60:
+    if sum(arr)/4>=60:
         print('pass')
         num += 1
     else:
