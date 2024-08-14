a6eeae037fc5a369343f1fff8499fcd0be08ed0b
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2024-08-15 00:25:18 +0900

    Title: 연속부분수열일까, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 240815/연속부분수열일까/contiguous-array-or-not.py
+++ 240815/연속부분수열일까/contiguous-array-or-not.py
@@ -1,20 +1,27 @@
 n1, n2 = map(int, input().split())
 arrA = list(map(int, input().split()))
 arrB = list(map(int, input().split()))
-idxs = []
-check=0
 
-for i in range(len(arrA)):
-    if arrB[0]==arrA[i]:
-        idxs.append(i)
-if len(idxs)!=0:
-    for el in idxs:
-        for j in range(len(arrB)):
-            if arrA[el+j]==arrB[j]:
-                check+=1
-        if check==len(arrB):
-            print('Yes')
-            break
-        check = 0
-if check==0:
+check=True
+cnts = []
+
+for i in range(len(arrB)):
+    cnts.append(arrA.count(arrB[i]))
+    if cnts[i]==0:
+        check=False
+
+if check:
+    for i in range(len(arrA)):
+        if arrA[i]==arrB[0]:
+            found = True
+            for j in range(1,len(arrB)):
+                if arrA[i+j]!=arrB[j]:
+                    found=False
+                    break
+            if found:
+                print('Yes')
+                break
+    if not found:
+        check = False
+if not check:
     print('No')
