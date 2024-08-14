c7c4e35ffa2a332b735eccb289031a91df1e15aa
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2024-08-14 23:07:44 +0900

    Title: 점수대 파악하기, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 240814/점수대 파악하기/find-out-the-score-range.py
+++ 240814/점수대 파악하기/find-out-the-score-range.py
@@ -4,7 +4,7 @@ for s in scores:
     if s==0:
         break
     if s//10==0:
-        pass
+        continue
     cntlst[s//10-1]+=1
 for i in range(9, -1, -1):
     print(f"{(i+1)*10} - {cntlst[i]}")
