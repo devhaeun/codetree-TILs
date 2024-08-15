862aec3ff0cab571af5beb04ac1e8a303bf422e3
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2024-08-15 18:07:45 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240815/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 240815/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -2,6 +2,6 @@ n = int(input())
 prices = list(map(int, input().split()))
 mini = min(prices[:n-1])
 idx = prices.index(mini)
-maxi = max(prices[idx+1:])
+maxi = max(prices[idx:])
 
 print(maxi-mini) if maxi>mini else print(0)
