858b214fe52eb6aac2efba8e8b5b926e9ca072e5
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2024-08-21 19:06:17 +0900

    Title: 특정 문자의 등장 횟수, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240821/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
+++ 240821/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -1,4 +1,9 @@
 string = input()
-ee = string.count('ee')
-eb = string.count('eb')
+ee=0
+eb=0
+for i in range(len(string)-1):
+    if string[i]=='e' and string[i+1]=='e':
+        ee+=1
+    if string[i]=='e' and string[i+1]=='b':
+        eb+=1
 print(ee, eb, sep=' ')
