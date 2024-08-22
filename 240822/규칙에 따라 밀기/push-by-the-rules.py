4dc3191e9c5534a947d9f3cfb1a45d1345eec33a
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2024-08-22 20:53:01 +0900

    Title: 규칙에 따라 밀기, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240822/규칙에 따라 밀기/push-by-the-rules.py
+++ 240822/규칙에 따라 밀기/push-by-the-rules.py
@@ -5,10 +5,12 @@ R = q.count('R')
 if L>R:
     left = L-R
     # 왼쪽으로 left 만큼 밀기
-    A = A[left:]+A[:left]
+    for _ in range(left):
+        A = A[1:]+A[0]
 elif R>L:
     right = R-L
     # 오른쪽으로 right 만큼 밀기
-    A = A[-1*right:]+A[:-1*right]
+    for _ in range(right):
+        A = A[-1]+A[:-1]
 
 print(A)
