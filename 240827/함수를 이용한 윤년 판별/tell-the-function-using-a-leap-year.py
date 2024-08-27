d17b5a4448f92cf215c848617411344a78276785
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2024-08-27 23:44:01 +0900

    Title: 함수를 이용한 윤년 판별, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240827/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -0,0 +1,13 @@
+y = int(input())
+
+def leap_year(n):
+    if n%4==0:
+        if n%100==0 and n%400!=0:
+            return False
+        return True
+    return False
+
+if leap_year(y):
+    print('true')
+else:
+    print('false')
