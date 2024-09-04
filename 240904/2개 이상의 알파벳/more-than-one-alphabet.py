b06657ce2910c8a4b40ac968f2c1f29d55239bba
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2024-09-04 23:43:30 +0900

    Title: 2개 이상의 알파벳, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 240904/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 240904/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -1,12 +1,15 @@
 string_a = input()
 
 def alpha_num(string):
+    alphabet = []
     cnt = 0
     for s in string:
-        if string.count(s)>=2:
+        if s not in alphabet:
+            alphabet.append(s)
             cnt+=1
             if cnt>=2:
                 print('Yes')
                 return
     print('No')
+
 alpha_num(string_a)
