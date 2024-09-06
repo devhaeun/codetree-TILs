e1eb8afc8842b035f18959149065366f516ef5bc
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2024-09-06 22:50:15 +0900

    Title: 숫자 차례로 출력하기, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240906/숫자 차례로 출력하기/to-output-numerically.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+def func1(n):
+    if n==0:
+        return
+    func1(n-1)
+    print(n, end=' ')
+
+def func2(n):
+    if n==0:
+        return
+    print(n, end=' ')
+    func2(n-1)
+
+func1(n)
+print()
+func2(n)
