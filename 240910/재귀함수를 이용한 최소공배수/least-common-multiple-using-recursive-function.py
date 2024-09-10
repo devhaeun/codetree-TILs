7c40014f358c9565d4f009e48832ec171805e26f
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2024-09-10 20:11:30 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240910/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -0,0 +1,25 @@
+n = int(input())
+numbers = list(map(int, input().split()))
+
+# 최소공배수 구하는 재귀함수
+def LCM(i):
+    if i==0:
+        result = 1
+    else:
+        result = LCM(i-1)
+    
+    if result%numbers[i]!=0:
+        if numbers[i] in [1,2,3,5,7]:
+            result *= numbers[i]
+        else:
+            if numbers[i]%2==0:
+                if result%4==0:
+                    result*=2
+                elif result%2==0:
+                    result*=numbers[i]//2
+            else:
+                result*=3 if result%3==0 else 9
+    return result
+
+result = LCM(n-1)
+print(result)
