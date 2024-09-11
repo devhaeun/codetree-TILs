c3fd2c72510a9970d3da8cee243a316cd373af2b
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2024-09-11 23:40:51 +0900

    Title: 오름 내림차순 정렬, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240911/오름 내림차순 정렬/inc-dec-sorting.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+arr.sort()
+for el in arr:
+    print(el, end=' ')
+print()
+
+arr.sort(reverse=True)
+for el in arr:
+    print(el, end=' ')
