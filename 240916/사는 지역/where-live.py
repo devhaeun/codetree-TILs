9e6f677b374afd3e235f8e9fb0dafb7bf1ebacf8
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2024-09-16 17:57:32 +0900

    Title: 사는 지역, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240916/사는 지역/where-live.py
+++ 240916/사는 지역/where-live.py
@@ -10,7 +10,7 @@ for _ in range(n):
     p_name, p_dst, p_loc = tuple(input().split())
     person1 = Person(p_name, p_dst, p_loc)
     if person1.name > temp:
-        temp = person1.name[0]
+        temp = person1.name
         person2 = Person(person1.name, person1.dst, person1.loc)
 
 print(f'name {person2.name}')
