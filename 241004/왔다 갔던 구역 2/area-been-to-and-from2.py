50a83f31c66ce67d6f77a60f101db54d2a0e13e7
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2024-10-04 18:59:51 +0900

    Title: 왔다 갔던 구역 2, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241004/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 241004/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,20 +1,25 @@
 n = int(input())
 arr = [0]*2001 # 위치 0 == arr[1000]
-arr[1000] = 1 # 시작 위치
+pos = 1000
 
 for _ in range(n):
-    pos = 1000
     x,D = input().split()
     x = int(x)
+    arr[pos]+=1
     if D=='R':
         for i in range(1,x+1):
-            arr[pos+i]+=1
+            pos+=1
+            arr[pos]+=1
+            # print(f'arr[{pos}]: {arr[pos]}')
     if D=='L':
         for i in range(1,x+1):
-            arr[pos-i]+=1
+            pos-=1
+            arr[pos]+=1
+            # print(f'arr[{pos}]: {arr[pos]}')
 
 # arr에서 0이 아닌 부분만 떼어오기
 arr = [x for x in arr if x!=0]
+# print(arr)
 
 result = 0
 for i in range(len(arr)-1):
